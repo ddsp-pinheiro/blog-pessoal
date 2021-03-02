e77260d92d2216262991332dc0aff0afb438d9b9
ddsp-pinheiro <ddsp.pinheiro@gmail.com>
2021-03-02 13:37:06 -0300

    feat(Model_usuarioLogin): adicionando atributos

---
+++ blogPessoal/src/main/java/org/generetion/blogPessoal/model/UsuarioLogin.java
@@ -0,0 +1,12 @@
+package org.generetion.blogPessoal.model;
+
+public class UsuarioLogin {
+
+	private String nome;
+	
+	private String usuario;
+	
+	private String senha;
+	
+	private String token;
+}

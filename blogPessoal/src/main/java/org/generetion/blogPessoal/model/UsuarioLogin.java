278e8b828ec2fbbb8bb5131e001677f498ad764a
ddsp-pinheiro <ddsp.pinheiro@gmail.com>
2021-03-02 13:40:32 -0300

    feat(Model_usuarioLogin): add getters e setters

--- blogPessoal/src/main/java/org/generetion/blogPessoal/model/UsuarioLogin.java
+++ blogPessoal/src/main/java/org/generetion/blogPessoal/model/UsuarioLogin.java
@@ -9,4 +9,40 @@ public class UsuarioLogin {
 	private String senha;
 	
 	private String token;
+
+	//--------------------------------------
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
 }

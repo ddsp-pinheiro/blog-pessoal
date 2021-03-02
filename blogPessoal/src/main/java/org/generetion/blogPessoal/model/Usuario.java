9649d09dbca1ffa9ab3a7e3ee1620826f5a15dca
ddsp-pinheiro <ddsp.pinheiro@gmail.com>
2021-03-02 13:30:12 -0300

    feat(Model_usuario): getters e setters

--- blogPessoal/src/main/java/org/generetion/blogPessoal/model/Usuario.java
+++ blogPessoal/src/main/java/org/generetion/blogPessoal/model/Usuario.java
@@ -12,7 +12,7 @@ import com.sun.istack.NotNull;
 @Entity
 @Table(name = "usuario")
 public class Usuario {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -28,5 +28,39 @@ public class Usuario {
 	@NotNull
 	@Size(min=5, max=100)
 	private String senha;
+	
+	//-------------------------------------------
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 }

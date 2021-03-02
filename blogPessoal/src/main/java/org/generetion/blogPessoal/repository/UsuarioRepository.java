efd014426b7228d6d7bbb55bded960b5f0b18918
ddsp-pinheiro <ddsp.pinheiro@gmail.com>
2021-03-02 13:48:06 -0300

    feat(UsuarioRepository): add metodo

---
+++ blogPessoal/src/main/java/org/generetion/blogPessoal/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package org.generetion.blogPessoal.repository;
+
+import java.util.Optional;
+
+import org.generetion.blogPessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	public Optional<Usuario> findByUsuario(String usuario);
+
+}
